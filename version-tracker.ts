7da48f2fe7dac9a151ce865ba21e1fa4ebb89536
Erik Thomsen <ethomsen86@gmail.com>
2025-03-17 10:53:53 -0400

    Updated

--- version-tracker.ts
+++ version-tracker.ts
@@ -1,7 +1,4 @@
 import { chromium } from 'playwright';
-import { Octokit } from '@octokit/rest';
-
-export let rawVersion: string = '';
 
 (async () => {
   const browser = await chromium.launch({ headless: true });
@@ -27,69 +24,19 @@ export let rawVersion: string = '';
   await page.goto(process.env.DOWNLOAD_PAGE_URL || '');
 
   // Grab the value from the table using xpath
-  const rawVersion = await page.$eval(
+  const version = await page.$eval(
     '//*[@id="introduction"]/table/tbody/tr[1]/td[2]',
     (element) => element.textContent?.trim() || ''
   );
 
-  const releaseDate = await page.$eval(
-    '//*[@id="introduction"]/table/tbody/tr[1]/td[3]',
-    (element) => element.textContent?.trim() || ''
-  );
+  // const releaseDate = await page.$eval(
+  //   '//*[@id="introduction"]/table/tbody/tr[1]/td[3]',
+  //   (element) => element.textContent?.trim() || ''
+  // );
 
-  console.log(`${rawVersion}`);
-  console.log(`${releaseDate}`);
+  console.log(`${version}`);
+  // console.log(`${releaseDate}`);
 
   // Close the browser
   await browser.close();
-})();
-
-const token = process.env.GITHUB_TOKEN; // GitHub token with repo permissions
-const owner = 'coretechonomy';
-const repo = 'version-tracker';
-
-if (!token) {
-  throw new Error('GITHUB_TOKEN environment variable is not set');
-}
-
-const octokit = new Octokit({
-  auth: token,
-});
-
-async function createTag(version: string) {
-  const { data: latestCommit } = await octokit.repos.getCommit({
-    owner,
-    repo,
-    ref: 'main',
-  });
-
-  await octokit.git.createTag({
-    owner,
-    repo,
-    tag: version,
-    message: `Release ${version}`,
-    object: latestCommit.sha,
-    type: 'commit',
-  });
-
-  await octokit.git.createRef({
-    owner,
-    repo,
-    ref: `refs/tags/${version}`,
-    sha: latestCommit.sha,
-  });
-
-  console.log(`Tag ${version} created successfully`);
-}
-
-(async () => {
-  try {
-    while (!rawVersion) {
-      await new Promise(resolve => setTimeout(resolve, 1000));
-    }
-    const version = rawVersion.split(' ')[0]; // Extract the version part
-    await createTag(version);
-  } catch (error) {
-    console.error(error);
-  }
 })();
